443ea98ee1f5a49457162db3410bc24b0fcedf77
sametirkoren <sametirkoren@gmail.com>
2020-07-14 23:34:55 +0300

    EntityBase mapping operation

--- Restaurant.DataAccess/Contexts/Restaurant/RestaurantContext.cs
+++ Restaurant.DataAccess/Contexts/Restaurant/RestaurantContext.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.Entity;
 using Restaurant.Entities.Tables;
+using Restaurant.Entities.Tables.Base;
 
 namespace Restaurant.DataAccess.Contexts.Restaurant
 {
@@ -23,5 +24,23 @@ namespace Restaurant.DataAccess.Contexts.Restaurant
         public DbSet<Tanim> Tanimlar { get; set; }
         public DbSet<Porsiyon> Porsiyonlar { get; set; }
         public DbSet<EkMalzeme> EkMalzemeler { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Types<EntityBase>().Configure(c =>
+            {
+                c.HasKey(e => e.Id);
+                c.Property(e => e.Ekleyen).HasMaxLength(30);
+                c.Property(e => e.Duzenleyen).HasMaxLength(30);
+                c.Property(e => e.Aciklama).HasMaxLength(200);
+
+                c.Property(e => e.Id).HasColumnName("Id");
+                c.Property(e => e.Duzenleyen).HasColumnName("Duzenleyen");
+                c.Property(e => e.Ekleyen).HasColumnName("Ekleyen");
+                c.Property(e => e.Aciklama).HasColumnName("Aciklama");
+                c.Property(e => e.DuzenlenmeTarihi).HasColumnName("DuzenlenmeTarihi");
+                c.Property(e => e.EklenmeTarihi).HasColumnName("EklenmeTarihi");
+            });
+        }
     }
 }
